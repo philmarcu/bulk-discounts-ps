853dfd7fa65fb566003cf4aea1b57f134927739a
philmarcu <philmark18@gmail.com>
2022-08-06 14:55:58 -0600

    Fix - add extra for index bulk_discount test

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'bulk discount index page' do
   describe 'I see all of my bulk discounts' do
-    it 'shows discounts & their info' do
+    it 'shows discounts & their info & links to show page' do
       merchant_1 = Merchant.create!(name: "Bobs Loggers")
 
       discount_1 = merchant_1.bulk_discounts.create!(name: "Bob's Special", qty_threshold: 10, pct_discount: 15)
@@ -12,19 +12,27 @@ RSpec.describe 'bulk discount index page' do
       visit "/merchants/#{merchant_1.id}/bulk_discounts"
 
       within "#disc-#{discount_1.id}" do
+        expect(page).to have_selector(:link_or_button, "Bob's Special Info.")
         expect(page).to have_content("Bob's Special - 15% Off for 10 Items!")
         expect(page).to_not have_content("Heavy Metal - 10% Off for 6 Items")
       end
 
       within "#disc-#{discount_2.id}" do
+        expect(page).to have_selector(:link_or_button, "Heavy Metal Info.")
         expect(page).to have_content("Heavy Metal - 10% Off for 6 Items")
         expect(page).to_not have_content("Beeeeeeees?!!??!! - 40% off all insects in store")
       end
 
       within "#disc-#{discount_3.id}" do
+        expect(page).to have_selector(:link_or_button, "Beeeeeeees?!!??!! Info.")
         expect(page).to have_content("Beeeeeeees?!!??!! - 40% Off for 20 Items!")
         expect(page).to_not have_content("Bob's Special - 15% Off 10 Items!")
       end
+
+      click_on "Bob's Special Info."
+
+      expect(page).to have_content("10 Items - Quantity Minimum for Discount")
+      expect(page).to_not have_content("6 Items - Quantity Minimum for Discount")
     end
   end
 end
