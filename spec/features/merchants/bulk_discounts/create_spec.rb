0fdd2db95c0ec1a178c1559d98424273d938b908
philmarcu <philmark18@gmail.com>
2022-08-06 19:02:20 -0600

    Feat- finalize create & new bulk discount user story

--- spec/features/merchants/bulk_discounts/create_spec.rb
+++ spec/features/merchants/bulk_discounts/create_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe 'bulk discounts new / create' do
     it 'renders the new form' do
       merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
 
-      visit "/merchants/#{merchant_1.id}/items/new"
+      visit "/merchants/#{merchant_1.id}/bulk_discounts/new"
 
-      expect(page).to have_content('New Item')
+      expect(page).to have_content('New Bulk Discount for Spongebob The Merchant')
       expect(find('form')).to have_content('Name')
       expect(find('form')).to have_content('Quantity Threshold')
       expect(find('form')).to have_content('Discount Percentage')
@@ -25,6 +25,23 @@ RSpec.describe 'bulk discounts new / create' do
 
       expect(page).to have_current_path("/merchants/#{merchant_1.id}/bulk_discounts")
       expect(page).to have_content("Current Offer: Super Saleeee!")
+    end
+
+    context 'non-valid data (name excluded)' do
+      it 'returns to new if pct_discount / qty_thres. not filled in' do
+        merchant_1 = Merchant.create!(name: "Bobs Loggers")
+
+        visit "/merchants/#{merchant_1.id}/bulk_discounts/new"
+
+        fill_in('Name', with: 'Super Saleeee!')
+        fill_in('Quantity Threshold', with: '')
+        fill_in('Discount Percentage', with: '')
+        click_on "Create"
+
+        expect(page).to have_current_path("/merchants/#{merchant_1.id}/bulk_discounts/new")
+        expect(page).to have_content("Error: Qty threshold can't be blank, Qty threshold is not a number")
+        expect(page).to have_content("Pct discount can't be blank, Pct discount is not a number")
+      end
     end
   end
 end
